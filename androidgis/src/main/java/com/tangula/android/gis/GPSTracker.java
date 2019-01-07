c5b9e0ce0ea37aa7654a726e8dff593a43166c63
wangdongshi <wangds2016@163.com>
2019-01-07 11:05:37 +0800

    允许自定义更新时间和距离

--- androidgis/src/main/java/com/tangula/android/gis/GPSTracker.java
+++ androidgis/src/main/java/com/tangula/android/gis/GPSTracker.java
@@ -56,10 +56,10 @@ public class GPSTracker {
 
 
     // The minimum distance to change Updates in meters
-    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
+    private static long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
 
     // The minimum time between updates in milliseconds
-    private static final long MIN_TIME_BW_UPDATES = 60000; // 1 minute
+    private static long MIN_TIME_BW_UPDATES = 60000; // 1 minute
 
     private static final List<CallbackFuncSet> CALLBACKS = new LinkedList<>();
 
